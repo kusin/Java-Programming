f86fc6f38decb547f618690a185cfeeeddabde61
kusin <aryajayaalamsyah@gmail.com>
2024-03-14 22:13:20 +0700

    -

--- Pertemuan3_array/src/pertemuan3_array/HitungGaji.java
+++ Pertemuan3_array/src/pertemuan3_array/HitungGaji.java
@@ -4,13 +4,12 @@
  */
 package pertemuan3_array;
 
-
-public class Perulangan {
+// main class
+public class HitungGaji {
+    
+    // main method
     public static void main(String[]args){
         
-        // 1, 2, 3, 4, 5
-        for(int i=1; i<5; i++){
-            System.out.println(i);
-        }
+        // deklarasi variabel
     }
 }
